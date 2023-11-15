f98c74f6dd2560f04933deb84fe0b2061429eca9
minimoocodes <jaemin.shim14@gmail.com>
2023-11-15 16:45:06 +0100

    Jinjing's advice fixed

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 let userYearEl = document.querySelector('#userYear');
 
+let zodiacForm = document.querySelector('#zodiacForm');
+zodiacForm.addEventListener('submit', zodiacCalc);
+
 function zodiacCalc(e) {
     e.preventDefault();
 
@@ -9,9 +12,12 @@ function zodiacCalc(e) {
     let userYearEl = Number(yearInputEl.value);
 
     let zodiac = ["Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat"];
-    let zodiacIndex = userYearEl %12;
+    let zodiacIndex = userYearEl % 12;
 
-    
+    const userZodiacResult = document.querySelector('#userZodiacResult');
+    userZodiacResult.textContent = `Your Oriental Zodiac is ${zodiac[zodiacIndex]}`
 }
+
+
 
 
