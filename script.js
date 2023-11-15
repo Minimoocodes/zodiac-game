97abb5ccfccbe704f68ed81116efdb842b4a61db
minimoocodes <jaemin.shim14@gmail.com>
2023-11-15 15:20:12 +0100

    clean up

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict'
 
 let userYearEl = document.querySelector('#userYear');
-// let submitBtn = document.querySelector('#button');
 
 function zodiacCalc(e) {
     e.preventDefault();
@@ -16,15 +15,3 @@ function zodiacCalc(e) {
 }
 
 
-// let year = Number(userYearEl.value);
-
-// let buttonEl = document.querySelector('#button');
-// buttonEl.addEventListener('submit', function(e){
-//     e.preventDefault();
-// });
-
-
-// let zodiac = ["Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat"];
-// let zodiacIndex = userYear % 12;
-
-// console.log(`Your Oriental Zodiac is ${zodiac[zodiacIndex]}`);
