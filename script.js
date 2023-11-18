1e6f4ae8cc29944276b634bf8181630c389cb3c8
minimoocodes <jaemin.shim14@gmail.com>
2023-11-18 21:19:52 +0100

    result modal fixed

--- script.js
+++ script.js
@@ -14,9 +14,23 @@ function zodiacCalc(e) {
     let zodiac = ["Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat"];
     let zodiacIndex = userYearEl % 12;
 
-    const userZodiacResult = document.querySelector('#userZodiacResult');
-    userZodiacResult.textContent = `Your Oriental Zodiac is ${zodiac[zodiacIndex]}`
+    let zodiacResultEl = document.querySelector('#zodiacResult');
+    zodiacResultEl.textContent = zodiac[zodiacIndex];
+
+    let zodiacImgEl = document.querySelector('#zodiacImg');
+    zodiacImgEl.src = "./img/"+ zodiacIndex + ".png";
+
+    let zodiacTableEl= document.querySelector('#zodiacTable');
+    zodiacTableEl.classList.add('image--hidden');
+    // const userZodiacResult = document.querySelector('#userZodiacResult');
+    // userZodiacResult.textContent = `Your Oriental Zodiac is ${zodiac[zodiacIndex]}`
 }
+
+let modalBtnEl = document.querySelector('#button');
+modalBtnEl.addEventListener('click', function(){
+    let modalMsg = document.querySelector('#zodiacModal');
+    modalMsg.classList.toggle('modal__inner--show');
+});
 
 
 
